694d6a344067f8b085b7db29999ba9ded69f5218
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2024-09-15 21:26:38 +0500

    circular linkedlist insertion not working right

--- circular_linkedlist.cpp
+++ circular_linkedlist.cpp
@@ -28,36 +28,71 @@ class Node{
 //     while(tail != NULL)
 //  }
 // insert 
-  void insert(int data, Node* tail){
-    // create a new Node
+  void insert(int data, Node* &tail, int element){
+
+        // create a new Node
      Node* newNode = new Node(data);
-     // create a pointer for reference
-     Node* temp = tail;
-
-     if(tail->next == NULL){
+ 
+     // if list is empty
+    if(tail == NULL){  
+       tail = newNode;
      tail->next = tail;
-
+     return;
+    
+    }
+     else{ 
+      // assuming that the list is not empty
+     // create a new reference
+     Node* curr = tail;
+     while( curr->data != element){
+        curr = curr->next;
      }
-     else if(tail-){
-        // create a pointer to hold the reference of tail->next
-        Node* forward = tail->next;
-        temp->next = newNode;
-        newNode->next = forward;
-        tail = newNode; // update tail
-     }
-     cout<<"data "<<newNode->data<<" next "<<newNode->next;
-    cout<<endl;
+        
+      
+        // Insert newNode after curr
+        newNode->next = curr->next;
+        curr->next = newNode;
 
-  }
+        // Update tail if newNode is inserted at the end
+        if (curr == tail){
+            tail = newNode;
+        }
+
+     
+    
+    }
+    
+}
+void print(Node* tail){
+       if (tail == NULL) {
+        cout << "Empty list" << endl;
+        return;
+    }
+
+    Node* temp = tail;
+    do{
+        cout<<tail->data<<" ";
+        tail = tail->next;
+    }while( tail != temp);
+  
+
+   cout<<endl;
+}
+
   
 
 
 int main(){
-    // crreate a node
+    Node* tail = NULL;
+    //    print(tail);
+    // create a node
     Node* node1 = new Node(10);
-    Node* tail = node1;
-    insert(20,tail);
-        insert(30,tail);
+   tail = node1;
+       print(tail);
+    insert(20,tail,10);
+       print(tail);
+        insert(30,tail,20);
+        print(tail);
     
 
     return 0;
